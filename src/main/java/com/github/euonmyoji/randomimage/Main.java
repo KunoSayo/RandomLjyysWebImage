549f446e1fdad1cea7afef7896c9a37b21f6c2a8
euOnmyoji <1418780411@qq.com>
2020-03-25 11:49:55 +0800

    push *

--- src/main/java/com/github/euonmyoji/randomimage/Main.java
+++ src/main/java/com/github/euonmyoji/randomimage/Main.java
@@ -3,9 +3,11 @@ package com.github.euonmyoji.randomimage;
 import javax.net.ssl.*;
 import java.io.Closeable;
 import java.io.IOException;
-import java.io.InputStream;
 import java.io.OutputStream;
+import java.net.ServerSocket;
+import java.net.Socket;
 import java.net.URLDecoder;
+import java.nio.charset.StandardCharsets;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
@@ -16,6 +18,7 @@ import java.util.Scanner;
 import java.util.concurrent.LinkedBlockingQueue;
 import java.util.concurrent.ThreadPoolExecutor;
 import java.util.concurrent.TimeUnit;
+import java.util.stream.Collectors;
 
 /**
  * @author yinyangshi
@@ -35,8 +38,13 @@ public class Main {
 
     public static void main(String[] args) throws Exception {
         System.setProperty("https.protocols", "TLSv1.2,TLSv1.1");
-        SSLContext sslContext;
+        ServerSocket serverSocket;
+        int port = 8080;
+        if (Files.exists(Paths.get("port.txt"))) {
+            port = Integer.parseInt(Files.newBufferedReader(Paths.get("port.txt")).readLine());
+        }
         if (Files.exists(Paths.get("./img.ljyys.ml/img.ljyys.ml.keystore"))) {
+            SSLContext sslContext;
             sslContext = SSLContext.getInstance("TLSv1");
             KeyStore keyStore = KeyStore.getInstance(KeyStore.getDefaultType());
             keyStore.load(Files.newInputStream(Paths.get("./img.ljyys.ml/img.ljyys.ml.keystore")), "123456".toCharArray());
@@ -49,21 +57,17 @@ public class Main {
             trustManagerFactory.init(keyStore);
 
             sslContext.init(keyManagerFactory.getKeyManagers(), trustManagerFactory.getTrustManagers(), null);
+            SSLServerSocketFactory socketFactory = sslContext.getServerSocketFactory();
+            serverSocket = socketFactory.createServerSocket(port);
+            ((SSLServerSocket) serverSocket).setEnabledProtocols(((SSLServerSocket) serverSocket).getSupportedProtocols());
+            ((SSLServerSocket) serverSocket).setEnabledCipherSuites(((SSLServerSocket) serverSocket).getSupportedCipherSuites());
         } else {
-            sslContext = SSLContext.getDefault();
+            serverSocket = new ServerSocket(port);
         }
-        SSLServerSocketFactory socketFactory = sslContext.getServerSocketFactory();
-        int port = 80;
-        if (Files.exists(Paths.get("port.txt"))) {
-            port = Integer.parseInt(Files.newBufferedReader(Paths.get("port.txt")).readLine());
-        }
-        SSLServerSocket serverSocket = (SSLServerSocket) socketFactory.createServerSocket(port);
-        serverSocket.setEnabledProtocols(serverSocket.getSupportedProtocols());
-        serverSocket.setEnabledCipherSuites(serverSocket.getSupportedCipherSuites());
-        SSLSocket socket;
+        Socket socket;
         while (running) {
             try {
-                while ((socket = (SSLSocket) serverSocket.accept()) != null) {
+                while ((socket = serverSocket.accept()) != null) {
                     socket.setKeepAlive(false);
                     socket.setSoTimeout(30000);
                     socket.setSoLinger(true, 30);
@@ -79,9 +83,9 @@ public class Main {
     }
 
     static class Request implements Runnable, Closeable {
-        private final SSLSocket socket;
+        private final Socket socket;
 
-        private Request(SSLSocket socket) {
+        private Request(Socket socket) {
             this.socket = socket;
         }
 
@@ -89,8 +93,10 @@ public class Main {
         public void run() {
             if (!socket.isClosed()) {
                 try {
-                    socket.setEnabledCipherSuites(socket.getSupportedCipherSuites());
-                    socket.startHandshake();
+                    if (socket instanceof SSLSocket) {
+                        ((SSLSocket) socket).setEnabledCipherSuites(((SSLSocket) socket).getSupportedCipherSuites());
+                        ((SSLSocket) socket).startHandshake();
+                    }
                     String child = "";
                     {
                         Scanner scanner = new Scanner(socket.getInputStream());
@@ -114,21 +120,24 @@ public class Main {
                     OutputStream out = socket.getOutputStream();
                     out.write("HTTP/1.1 200 OK\r\n".getBytes());
                     out.write("Server: ljyys/1.0\r\n".getBytes());
+                    out.write("Accept-Ranges: bytes\r\n".getBytes());
+                    out.write("Connection: Close\r\n".getBytes());
                     Path image = images[new Random().nextInt(images.length)];
-                    try (InputStream fin = Files.newInputStream(image)) {
-                        out.write(("Content-Length: " + fin.available() + "\r\n").getBytes());
-                        out.write(("Content-Type: image/" + image.getFileName().getFileName().toString().split("\\.", 2)[1] + "\r\n").getBytes());
-                        out.write("Accept-Ranges: bytes\r\n".getBytes());
-                        out.write("Connection: Close\r\n".getBytes());
-                        out.write("\r\n".getBytes());
-                        out.flush();
-                        byte[] tmp = new byte[8 * 1024];
-                        int len;
-                        while ((len = fin.read(tmp)) != -1) {
-                            out.write(tmp, 0, len);
-                            out.flush();
-                        }
-                    }
+//                    try (InputStream fin = Files.newInputStream(image)) {
+//                        out.write(("Content-Length: " + fin.available() + "\r\n").getBytes());
+//                        out.write(("Content-Type: image/" + image.getFileName().getFileName().toString().split("\\.", 2)[1] + "\r\n").getBytes());
+//
+//                        out.write("\r\n".getBytes());
+//                        out.flush();
+//                        byte[] tmp = new byte[8 * 1024];
+//                        int len;
+//                        while ((len = fin.read(tmp)) != -1) {
+//                            out.write(tmp, 0, len);
+//                            out.flush();
+//                        }
+//                    }
+                    out.write("\r\n".getBytes());
+                    out.write(Files.list(Paths.get(".")).collect(Collectors.toList()).toString().getBytes(StandardCharsets.UTF_8));
                 } catch (IOException e) {
                     System.err.println("[ERROR]" + e);
                     System.err.flush();
